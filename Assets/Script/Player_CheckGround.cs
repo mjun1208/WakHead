08e015f4eeb40e8e9ed727765b1460e06610ee71
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2020-04-23 22:10:09 +0900

    no message

---
+++ Assets/Script/Player_CheckGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_CheckGround : MonoBehaviour
+{
+    public PlayerMovement player;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "SkyBlock")
+        {
+            player.IsGround = true;
+            player.JumpCount = 0;
+        }
+        else
+        {
+            player.IsGround = false;
+            player.JumpCount = 0;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "SkyBlock")
+            player.IsGround = false;
+    }
+}
