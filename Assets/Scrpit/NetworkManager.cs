826e145288c99b62fb179b7670c6aebe4a04df0d
mjun1208 <mjun1208@gmail.com>
2020-07-02 15:25:33 +0900

    no message

---
+++ Assets/Scrpit/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void JoinGame()
+    {
+        StartCoroutine(ConnectToServer());
+    }
+
+    IEnumerator ConnectToServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+    }
+}
---
+++ Assets/Scrpit/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool IsLocalPlayer = false;
+    Vector2 OldPos;
+    Vector2 CurPos;
+
+    public float MoveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurPos = this.transform.position;
+        OldPos = CurPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsLocalPlayer) {
+            return;
+        }
+
+        Move();
+    }
+    void Move()
+    {
+        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * MoveSpeed;
+
+        transform.Translate(dir.x, dir.y, 0);
+
+        CurPos = this.transform.position;
+
+        if (CurPos != OldPos)
+        {
+            //네트워크에 적용
+            OldPos = CurPos;
+        }
+    }
+}
