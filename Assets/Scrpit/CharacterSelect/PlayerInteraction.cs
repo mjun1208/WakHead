6515a138cb1dd25cdde68f2e1594042ba305c917
방저녁 <hyeok020514@naver.com>
2020-10-13 11:59:23 +0900

    캐릭터 선택 창 지금이라도 올리겠읍니다

---
+++ Assets/Scrpit/CharacterSelect/PlayerInteraction.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject CharacterSlots;
+
+    public GameObject CharacterIllust;
+    public Sprite[] CharacterIllusts;
+    private Image CharacterIllust_Image;
+    private Animator CharacterIllust_Animator;
+
+    public Text CharacterName;
+    public Text CharacterDescription;
+    public string[] CharacterNames;
+    public string[] CharacterDescriptions;
+
+    private int CurrentCursor = 0;
+    private int AnimationCursor = -1;
+
+    public Toggle[] Slots;
+    
+
+    void Start()
+    {
+        CharacterIllust_Image = CharacterIllust.GetComponent<Image>();
+        CharacterIllust_Animator = CharacterIllust.GetComponent<Animator>();
+
+        int Count = 0;
+        foreach (Transform child in CharacterSlots.transform)
+        {
+            Slots[Count] = child.GetComponent<Toggle>();
+                Count++;
+        }
+    }
+    
+
+    void Update()
+    {
+        KeyInput();
+
+        Slots[CurrentCursor].isOn = true;
+
+        if (CurrentCursor != AnimationCursor)
+        {
+            CharacterIllust_Animator.Rebind();
+            CharacterIllust_Image.sprite = CharacterIllusts[CurrentCursor];
+            AnimationCursor = CurrentCursor;
+
+            CharacterName.text = CharacterNames[CurrentCursor];
+            CharacterDescription.text = CharacterDescriptions[CurrentCursor];
+        }
+    }
+
+    void KeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (CurrentCursor > 0)
+                CurrentCursor--;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (CurrentCursor < Slots.Length -1 && Slots[CurrentCursor + 1] != null)
+                CurrentCursor++;
+        }
+    }
+
+}
